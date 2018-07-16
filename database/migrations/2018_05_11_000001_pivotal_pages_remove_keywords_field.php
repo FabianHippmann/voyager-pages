b763e3f132f1c304805dd26cad40b8502f247a36
FabianHippmann <fabian.hippmann@moonshiner.at>
2018-07-16 11:16:21 +0200

    add namespacing for pages migration

--- database/migrations/2018_05_11_000001_pivotal_pages_remove_keywords_field.php
+++ database/migrations/2018_05_11_000001_pivotal_pages_remove_keywords_field.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RemoveKeywordsField extends Migration
+class PivotalPagesRemoveKeywordsField extends Migration
 {
     /**
      * Run the migrations.
